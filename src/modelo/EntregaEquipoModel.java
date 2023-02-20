be3feea160e456de477750a5a90fdb7aaef4cc3e
Usuario <Usuario@DESKTOP-BP5PTF8>
2023-02-20 12:09:00 -0600

    Parte final

--- src/modelo/EntregaEquipoModel.java
+++ src/modelo/EntregaEquipoModel.java
@@ -1,6 +1,19 @@
 package modelo;
 
+import java.util.ArrayList;
+
 
 public class EntregaEquipoModel {
+   public ArrayList<EntregaEquipoModel> DatosPersona   = new ArrayList<EntregaEquipoModel>();
    
+   public void IngresoPersona(String Apellidos, String Nombre, String Telefono){
+       
+        EntregaEquipoModel NuevaEntrega = new EntregaEquipoModel();
+        this.DatosPersona.add(NuevaEntrega);
+            }
+
+            public ArrayList EntregaEquipoModel()
+            {
+                return EntregaEquipoModel();
+            }
 }
--- src/modelo/NombrePersona.java
+++ src/modelo/NombrePersona.java
@@ -1,5 +1,39 @@
 package modelo;
 
-public class NombrePersona {
+public class NombrePersona{
+        String nombre;
+        String apellido;
+        String telefono;
+        
+        public NombrePersona(String nombre, String apellido, String telefono){
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.telefono = telefono;
+            
+        }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     
 }
